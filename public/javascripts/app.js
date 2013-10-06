36944c07ab0ade08f19297224e8d111bdaa2ac8a
Chris Roth <chris@cjroth.com>
2013-10-07 00:31:43 +0200

    make sections editable

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,12 +1,11 @@
 (function() {
 
 
-  $('.contract-sections, .available-sections').sortable({ connectWith: '.section-list' });
+  $('.section-list').sortable({ connectWith: '.section-list' });
 
   $('.modal .btn-submit').click(function() {
     var $modal = $(this).parents('.modal');
     $modal.find('form').submit();
-    $modal.modal('hide');
   });
 
   $('.company').text('Company');
@@ -15,11 +14,47 @@
     var val = $('#edit-company [name="company"]').val();
     if (!val.length) return false;
     $('.company').text(val);
+    var $modal = $(this).parents('.modal');
+    $modal.modal('hide');
     return false;
   });
 
   $('.company').click(function() {
     $('#edit-company').modal('show');
+    setTimeout(function() {
+      $('#name').focus();
+    }, 500);
+  });
+
+  var insideSectionClick, outsideSectionClick;
+
+  var startEditing = function($e, event) {
+    $('.section-list').removeClass('sortable').addClass('editing');
+    $e.addClass('editing');
+    $('.section-list').sortable('disable');
+    $e.attr('contentEditable', 'true');
+    $e.focus();
+    insideSectionClick = function(e) {
+      e.stopPropagation();
+    };
+    outsideSectionClick = function(e) {
+      stopEditing($e, e);
+    };
+    $e.on('click', insideSectionClick);
+    $('body').on('click', outsideSectionClick);
+  };
+
+  var stopEditing = function($e, event) {
+    $('.section-list').addClass('sortable').removeClass('editing');
+    $e.removeClass('editing');
+    $('.section-list').sortable('enable');
+    $e.attr('contentEditable', 'false');
+    $('body').off('click', outsideSectionClick);
+    $e.off('click', insideSectionClick);
+  };
+
+  $('.section').dblclick(function(e) {
+    startEditing($(this), e);
   });
 
 })();
